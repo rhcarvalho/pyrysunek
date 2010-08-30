0dad74968aafef5c0f6ed128d49257e698215950
Rodolfo Carvalho <rhcarvalho@gmail.com>
2010-08-30 10:15:40 -0300

    Add basic test infrastructure.

--- rysunek.py
+++ rysunek.py
@@ -7,10 +7,34 @@ from OpenGL.GLUT import *
 from OpenGL.GL import *
 from sys import argv
 
-def init ():
+
+class App(object):
+    def __init__(self):
+        self.__objects = []
+
+    def getToolbar(self):
+        return Toolbar()
+        
+    def onMouseEvent(self, button, state, x, y):
+        if state == GLUT_UP and x and y:
+            self.__objects.append(None)
+        
+    def getObjects(self):
+        return self.__objects
+
+class Toolbar:
+    def getButton(self, index):
+        return Button()
+        
+class Button:
+    def click(self):
+        return True
+        
+
+def init():
     "Set up several OpenGL state variables"
     # Background color
-    glClearColor (0.0, 0.0, 0.0, 0.0)
+    glClearColor(0.0, 0.0, 0.0, 0.0)
     # Projection matrix
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
@@ -19,22 +43,26 @@ def init ():
 def display():
     "Does the actual drawing"
     # Clear frame buffer
-    glClear (GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT);
     # Set draw color to white
-    glColor3f (1.0, 1.0, 1.0)
+    glColor3f(1.0, 1.0, 1.0)
     # Draw square
     glBegin(GL_POLYGON)
     for v in [[0.3,0.3],[0.7, 0.3],[0.7, 0.7],[0.3, 0.7]]:
-        glVertex2fv (v)
+        glVertex2fv(v)
     glEnd()
     # Flush and swap buffers
     glutSwapBuffers()
+    
 
-# Main Program
-glutInit(argv) 
-glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE)
-glutInitWindowSize(400, 400)
-glutCreateWindow("Square")
-glutDisplayFunc(display)
-init()
-glutMainLoop()
+if __name__ == "__main__":
+    # Main Program
+    glutInit(argv) 
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE)
+    glutInitWindowSize(400, 400)
+    glutCreateWindow("PyRysunek")
+    glutDisplayFunc(display)
+    app = App()
+    glutMouseFunc(app.onMouseEvent)
+    init()
+    glutMainLoop()
---
+++ rysunek_test.py
@@ -0,0 +1,35 @@
+import unittest
+import rysunek
+from OpenGL.GLUT import *
+
+
+class TestToolbar(unittest.TestCase):
+    def testClickButton1(self):
+        app = rysunek.App()
+        toolbar = app.getToolbar()
+        button1 = toolbar.getButton(1)
+        self.assertEqual(button1.click(), True)
+
+class RectangleTest(unittest.TestCase):
+    def testCannotCreateEmptyRectangle(self):
+        app = rysunek.App()
+        app.onMouseEvent(GLUT_LEFT_BUTTON, GLUT_DOWN, 0, 0) # click
+        app.onMouseEvent(GLUT_LEFT_BUTTON, GLUT_UP, 0, 0) # release
+        self.assertEqual(len(app.getObjects()), 0)
+        
+    def testCanCreateSimpleRectangle(self):
+        app = rysunek.App()
+        app.onMouseEvent(GLUT_LEFT_BUTTON, GLUT_DOWN, 0, 0) # click
+        app.onMouseEvent(GLUT_LEFT_BUTTON, GLUT_UP, 10, 10) # release
+        self.assertEqual(len(app.getObjects()), 1)
+        
+    def testCanCreateTwoRectangles(self):
+        app = rysunek.App()
+        app.onMouseEvent(GLUT_LEFT_BUTTON, GLUT_DOWN, 10, 10) # click
+        app.onMouseEvent(GLUT_LEFT_BUTTON, GLUT_UP, 20, 30) # release
+        app.onMouseEvent(GLUT_LEFT_BUTTON, GLUT_DOWN, 40, 40) # click
+        app.onMouseEvent(GLUT_LEFT_BUTTON, GLUT_UP, 50, 60) # release
+        self.assertEqual(len(app.getObjects()), 2)
+
+if __name__ == "__main__":
+    unittest.main()
