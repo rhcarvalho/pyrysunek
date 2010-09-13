ec3bff17458cb1ff2423c25b4ff544af15951833
Rodolfo Carvalho <rhcarvalho@gmail.com>
2010-09-13 00:26:02 -0300

    Move OpenGL code to inside App class.

--- rysunek.py
+++ rysunek.py
@@ -5,7 +5,7 @@ Draws a white square on a black background
 import OpenGL
 from OpenGL.GLUT import *
 from OpenGL.GL import *
-from sys import argv
+import sys
 
 
 class App(object):
@@ -17,7 +17,35 @@ class App(object):
         self.__objects = []
         self.toolbar = Toolbar()
         self.line_size = True
-
+        
+        self._init_opengl()
+        
+    def _init_opengl(self):
+        glutInit(sys.argv)
+        glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE)
+        glutInitWindowSize(400, 400)
+        glutCreateWindow("PyRysunek")
+        
+        glutDisplayFunc(self.display)
+        glutMouseFunc(self.on_mouse_event)
+        
+        # Background color
+        glClearColor(1.0, 1.0, 1.0, 0.0)
+        # Projection matrix
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0)
+    
+    def display(self):
+        "Does the actual drawing"
+        # Clear frame buffer
+        glClear(GL_COLOR_BUFFER_BIT)
+        
+        self.toolbar.draw()
+        
+        # Flush and swap buffers
+        glutSwapBuffers()
+        
     def on_mouse_event(self, button, state, x, y):
         """Handle mouse events."""
         if self.toolbar.contains(x, y):
@@ -67,40 +95,17 @@ class Toolbar:
             self.selected_tool = True
         else:
             self.selected_tool = self.SELECTION_TOOL
-        
-
-def init():
-    "Set up several OpenGL state variables"
-    # Background color
-    glClearColor(0.0, 0.0, 0.0, 0.0)
-    # Projection matrix
-    glMatrixMode(GL_PROJECTION)
-    glLoadIdentity()
-    glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0)
-
-def display():
-    "Does the actual drawing"
-    # Clear frame buffer
-    glClear(GL_COLOR_BUFFER_BIT);
-    # Set draw color to white
-    glColor3f(1.0, 1.0, 1.0)
-    # Draw square
-    glBegin(GL_POLYGON)
-    for v in [[0.3,0.3],[0.7, 0.3],[0.7, 0.7],[0.3, 0.7]]:
-        glVertex2fv(v)
-    glEnd()
-    # Flush and swap buffers
-    glutSwapBuffers()
-
+    
+    def draw(self):
+        # Set draw color
+        glColor3f(1.0, 0.0, 0.0)
+        # Draw square
+        glBegin(GL_POLYGON)
+        for v in ((0, 0), (0.1, 0), (0.1, 0.1), (0, 0.1)):
+            glVertex2fv(v)
+        glEnd()
 
 if __name__ == "__main__":
     # Main Program
-    glutInit(argv)
-    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE)
-    glutInitWindowSize(400, 400)
-    glutCreateWindow("PyRysunek")
-    glutDisplayFunc(display)
     app = App(debug=True)
-    glutMouseFunc(app.on_mouse_event)
-    init()
     glutMainLoop()
